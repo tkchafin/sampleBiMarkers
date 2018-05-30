708c49189d0e4b85cd671d22753c2801f13215f8
Tyler K. Chafin <tkchafin@uark.edu>
2018-05-30 15:05:25 -0500

    functions to random sample alleles from given set

--- seq_tools.py
+++ seq_tools.py
@@ -3,8 +3,43 @@
 import re
 import sys
 import os
+import random
+import operator
 
 """Tools for manipulating sequence data by Tyler Chafin"""
+
+#function to sample non-N alleles without replacement
+def sampleAlleles(geno, n, allowN, allowG):
+	expanded = list()
+	for gen in geno:
+		for nuc in get_iupac_caseless_diploid(gen):
+			if not allowN and nuc in ("N", "n"):
+				continue
+			if not allowG and nuc == "-":
+				continue
+			expanded.append(nuc)
+	if n > len(expanded):
+		return(expanded)
+	else:
+		ret = list()
+		for sampled in sampleList(expanded, n):
+			ret.append(sampled)
+		return(ret)
+
+
+#utility function to randomly sample w/out replacement from a list
+def sampleList(stuff, r):
+	rand = random.random
+	n = len(stuff)
+	pop = len(stuff)
+	for samp in range(r, 0, -1):
+		cumprob=1.0
+		x=rand()
+		while x < cumprob:
+			cumprob -= cumprob*samp/pop
+			pop-=1
+		yield stuff[n-pop-1]
+
 
 #function to check if N content in list of diploid genotypes (1 element = 1 pair of alleles)
 #is greater than a given threshold
@@ -39,20 +74,20 @@ def checkNcontent(nucs, threshold):
 #2 = het
 #? = - or N
 def nucs2numeric(nucs):
+	ret = list()
+	counts = {"A":0, "G":0, "C":0, "T":0}
+	#find major allele
+	for nuc in nucs:
+		nuc = nuc.upper()
+		if nuc not in ("-", "N", "n"):
+			counts[nuc]+=1
+
 	if isBiallelic(nucs):
 		#print(nucs)
-		ret = list()
-		counts = {"A":0, "G":0, "C":0, "T":0}
-		#find major allele
-		for nuc in nucs:
-			if nuc not in ("-", "N"):
-				for exp in get_iupac_caseless(nuc):
-					counts[exp] += 1
 		#sort dict, to list of tuples (b/c dicts are orderless, can't keep as dict)
 		sorted_x = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
 		majA = sorted_x[0][0]
 		minA = sorted_x[1][0]
-		het = reverse_iupac(''.join(sorted(set([majA, minA])))) #get het code
 		#print(majA, minA, het)
 
 		for nuc in nucs:
@@ -61,56 +96,19 @@ def nucs2numeric(nucs):
 				ret.append("0")
 			elif nuc == minA:
 				ret.append("1")
-			elif nuc == het:
-				ret.append("2")
 			elif nuc == "-":
 				ret.append("-")
 			else:
 				ret.append("?")
 
 		return(ret)
-	else:
-		print("Warning: Data is not biallelic:",nucs)
-		return(None)
-
-#Function takes biallelic list of nucleotides and converts to numeric
-#0 = major allele
-#1 = minor allele
-#2: Randomly samples heterozygous sites as 0 or 1
-def nucs2numericNohet(nucs):
-	if isBiallelic(nucs):
-		#print(nucs)
-		ret = list()
-		counts = {"A":0, "G":0, "C":0, "T":0}
-		#find major allele
+	elif isMonomorphic(nucs):
 		for nuc in nucs:
-			if nuc not in ("-", "N"):
-				for exp in get_iupac_caseless(nuc):
-					counts[exp] += 1
-		#sort dict, to list of tuples (b/c dicts are orderless, can't keep as dict)
-		sorted_x = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
-		majA = sorted_x[0][0]
-		minA = sorted_x[1][0]
-		het = reverse_iupac(''.join(sorted(set([majA, minA])))) #get het code
-		#print(majA, minA, het)
-
-		for nuc in nucs:
-			nuc = nuc.upper()
-			if nuc == majA:
-				ret.append("0")
-			elif nuc == minA:
-				ret.append("1")
-			elif nuc == het:
-				ret.append(random.randint(0,1))
-			elif nuc == "-":
-				ret.append("-")
-			else:
-				ret.append("?")
-
+			ret.append("0")
 		return(ret)
 	else:
-		print("Warning: Data is not biallelic:",nucs)
 		return(None)
+
 
 #Function to translate a string of bases to an iupac ambiguity code
 def reverse_iupac(char):
@@ -135,6 +133,24 @@ def reverse_iupac(char):
 		'ACGT':'N'
 	}
 	return iupac[char]
+
+#Function takes a list of nucleotides, and returns True if the column is monomorphic
+#ignores gaps and Ns
+#expands uipac codes using a call to external function
+def isMonomorphic(nucs):
+	expanded = list()
+	for nuc in nucs:
+		if nuc not in ("-", "N"):
+			for exp in get_iupac_caseless(nuc):
+				expanded.append(exp)
+	uniq_sort = sorted(set(expanded))
+	if len(uniq_sort) != 1:
+		#print(nucs)
+		#print(uniq_sort, len(uniq_sort))
+		return(False)
+	else:
+		return(True)
+
 
 #Function takes a list of nucleotides, and returns True if the column is biallelic
 #ignores gaps and Ns
@@ -178,7 +194,30 @@ def get_iupac_caseless(char):
 	ret = iupac[char]
 	return ret
 
-
+#Function to split character to IUPAC codes, returning diploid genotype
+def get_iupac_caseless_diploid(char):
+	if char.islower():
+		char = char.upper()
+	iupac = {
+		"A"	: ["A", "A"],
+		"G"	: ["G", "G"],
+		"C"	: ["C", "C"],
+		"T"	: ["T", "T"],
+		"N"	: ["N", "N"],
+		"-"	: ["-","-"],
+		"R"	: ["A","G"],
+		"Y"	: ["C","T"],
+		"S"	: ["G","C"],
+		"W"	: ["A","T"],
+		"K"	: ["G","T"],
+		"M"	: ["A","C"],
+		"B"	: ["C","G","T"],
+		"D"	: ["A","G","T"],
+		"H"	: ["A","C","T"],
+		"V"	: ["A","C","G"]
+	}
+	ret = iupac[char]
+	return ret
 
 #Goes through a dict of sequences and get the alignment length
 def getSeqLen(aln):
